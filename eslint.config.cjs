18b9baa74619d9ee85ff7a6c4350b5a2682e8d1f
timgardd <gardashtim@gmail.com>
2026-02-25 17:40:26 +0100

    chore: setup eslint, prettier and husky

---
+++ eslint.config.cjs
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-undef": "error"
+    }
+  }
+];

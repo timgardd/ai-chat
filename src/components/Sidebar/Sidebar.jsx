bfe27b183593631fce3872238585efa05c283460
timgardd <gardashtim@gmail.com>
2026-05-03 21:46:43 +0200

    fix: phone layout adaptation

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ export default async function Sidebar() {
   const conversations = await getConversations();
 
   return (
-    <div className="w-64 bg-gray-900 text-white flex flex-col border-r border-gray-800">
+    <div className="w-full md:w-64 h-1/3 md:h-full shrink-0 bg-gray-900 text-white flex flex-col border-b md:border-b-0 md:border-r border-gray-800">
       <SidebarClient initialConversations={conversations} />
     </div>
   );
